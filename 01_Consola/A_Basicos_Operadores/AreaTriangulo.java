b7ecc7cbdfcbc69d77ee317c28d4ba43c201e0b2
Rodrigo Alexis Pineda Alvarez <ale@MacBook-Air-de-Rodrigo.local>
2026-08-01 00:54:04 -0600

    Ejercicio AreaTriangulo resuelto

--- 01_Consola/A_Basicos_Operadores/AreaTriangulo.java
+++ 01_Consola/A_Basicos_Operadores/AreaTriangulo.java
@@ -1,14 +1,13 @@
-/**
- * Reto 9: Área de un Triángulo
- * Descripción: Solicitar la base y altura, y calcular el área.
- */
 import java.util.Scanner;
 
 public class AreaTriangulo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar base y altura
-        // TODO: Calcular el área ((base * altura) / 2)
-        // TODO: Imprimir resultado
+        System.out.println("Ingresa la base:");
+        double base = teclado.nextDouble();
+        System.out.println("Ingresa la altura:");
+        double altura = teclado.nextDouble();
+        double area = (base * altura) / 2;
+        System.out.println("El area es: " + area);
     }
 }
